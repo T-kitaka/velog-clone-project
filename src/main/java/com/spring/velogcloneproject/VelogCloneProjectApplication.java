3dadef8cfced708a5fe379fbfea78d48f25efee7
Jimin730 <dnddnd120@naver.com>
2025-03-15 23:45:46 +0900

    Initial commit

---
+++ src/main/java/com/spring/velogcloneproject/VelogCloneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.velogcloneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelogCloneProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelogCloneProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/velogcloneproject/VelogCloneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.velogcloneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelogCloneProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
